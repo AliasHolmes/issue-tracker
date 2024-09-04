195992cd17e9fdfd1ec649514093975c8aed7c3a
alias <zarifhosen2015@gmail.com>
2024-09-04 18:17:12 +0600

    font not working

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@radix-ui/themes/styles.css";
-import { Theme, ThemePanel } from "@radix-ui/themes";
+import "./theme-config.css";
+import { Theme } from "@radix-ui/themes";
 import "./globals.css";
 import Navbar from "./Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.variable}>
         <Theme accentColor="cyan" grayColor="slate" radius="large">
           <Navbar />
           <main className="p-5">{children}</main>
