e159d93489419972765ae5fb0d269fa88d1345e6
Alias <zarifhosen2015@gmail.com>
2024-09-18 12:31:47 +0600

    Refactor loading components to use Skeleton for better user experience

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { Box, Card } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingIssueDetailPage = () => {
-  return <div>loading...</div>;
+  return (
+    <Box className="max-w-xl">
+      <Skeleton />
+      <div className="flex space-x-3 my-2 mb-4">
+        <Skeleton width="5rem" />
+        <Skeleton width="8rem" />
+      </div>
+      <Card className="prose mt-4">
+        <Skeleton count={3} />
+      </Card>
+    </Box>
+  );
 };
 
 export default LoadingIssueDetailPage;
